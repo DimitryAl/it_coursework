5c3f10b09f1f689f481ea0e74e905fb71d3ec4d1
Dmitry Alexeev <dalexeev.work@gmail.com>
2021-06-08 14:29:36 +0300

    fixed mistake

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,6 +3,8 @@ import {Switch, Route, Redirect} from 'react-router-dom'
 import {Booklist} from './pages/Booklist'
 import {Addbook} from './pages/Addbook'
 import {Myinfo} from './pages/Info'
+import {Edit} from './pages/Editbook'
+
 
 export const useRoutes = () => {
     return (
@@ -15,6 +17,8 @@ export const useRoutes = () => {
             </Route>
             <Route path='/myinfo' exact>
                 <Myinfo />
+            </Route>
+            <Route path='/edit' render={props => (<Edit {...props}/>)} exact> 
             </Route>
             <Redirect to="/books"/>
         </Switch>
